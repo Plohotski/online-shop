d99efd37600561e6dbbecaffd1fa7562c0a5fa1c
Plohotski <plohoc@gmail.com>
2022-10-02 02:39:43 +0300

    feat: add component product

--- shop/src/components/Product.tsx
+++ shop/src/components/Product.tsx
@@ -9,6 +9,8 @@ interface ProductProps {
 export function Product({product}: ProductProps) {
   const [details, setDetails] = useState(false)
 
+  const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+  const btnClasses = ['border py-2 px-4', btnBgClassName];
   return(
     <div
     className="border py-2 px-4 rounded flex flex-col items-center mb-2">
@@ -16,13 +18,14 @@ export function Product({product}: ProductProps) {
       <p>{product.title}</p>
       <p className="font-bold">{product.price}</p>
       <button 
-      className="py-2 px-4 border bg-yellow-400"
+      className={btnClasses.join(' ')}
       onClick={() => {setDetails(prev => !prev)}}
       >
         {details ? 'Hide Details' : 'Show Details'}
       </button>
       {details && <div>
         <p>{product.description}</p>
+        <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
       </div>}
     </div>
   )
